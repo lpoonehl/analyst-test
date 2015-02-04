17acefc59b1349fcf77c6a17a4c49c331f373858
lpoonehl <pooneh.pordeli@gmail.com>
2015-02-04 05:28:35 -0700

    Question 6
    Question 6

--- Question6.R
+++ Question6.R
@@ -2,13 +2,14 @@
 # Question 6 #
 ##############
 
-#sink("Question6.txt", append=TRUE)
+sink("Question61.txt", append=TRUE)
 
 # Read the dataset from the path that it has been saved as a "csv" file; 
 #(Different in each computer); 
 data<-read.csv("/Users/pooneh/Desktop/analyst-data.csv", header=TRUE)
 #attach(data)
 
+library(reshape)
 data <- rename(data, c(v1="normal", v2="poisson", v3="gauss"))
 names(data) <- c("id", "time", "cats", "normal", "poisson", "gauss")
 
@@ -17,7 +18,8 @@ names(data) <- c("id", "time", "cats", "normal", "poisson", "gauss")
 # Create new variable "cru";
 set.seed(2015)
 N=nrow(data)
-cru=rnorm(N,24,10)
+cru=c(rnorm((N/3),33,10),rnorm((N/3),34,10),rnorm((N/3),5,10))
+
 
 # Add the new variable "cru" to the existing dataset;
 new.data=data.frame(data,cru)
@@ -30,12 +32,12 @@ print(head(new.data))
 # To see the effect of "time" as a fixed categorical effect on the 
 # variable "cru" cosidering "id" as the random effect, 
 # Use "lme";
-install.packages("nlme")
+#install.packages("nlme")
 library(nlme)
-model3=lme(cru~factor(time)-1, random=~1|id)
+model3=lme(cru~factor(time)-1, new.data, random=~1|id)
 s3=summary(model3)
 print(s3)
 #anova(model3)
 
-#sink()
+sink()
 
